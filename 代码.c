ef7e1364d3a9bb093021e1bb02fc5bc88ed55b37
Xilu-H <64730664+Xilu-H@users.noreply.github.com>
2020-05-05 00:29:38 +0800

    Add files via upload

---
+++ 代码.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+	int a[]={1,2,3,4};
+	int *b=NULL;
+	b=a;
+	printf("%d\n",sizeof(a)/sizeof(a[0]));
+	printf("%d\n",sizeof(b)/sizeof(b[0]));
+	for(int i=0;i<4;i++){
+		printf("%d",b[i]);
+	}
+	return 0;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
