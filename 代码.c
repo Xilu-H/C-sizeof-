086ddce1e6489bab81b9ef39e22f85627fadc45a
Xilu-H <64730664+Xilu-H@users.noreply.github.com>
2020-05-05 22:21:09 +0800

    Update 代码.c

--- 代码.c
+++ 代码.c
@@ -2,11 +2,12 @@
 
 int main(){
 	int a[]={1,2,3,4};
+	int i;
 	int *b=NULL;
 	b=a;
 	printf("%d\n",sizeof(a)/sizeof(a[0]));
 	printf("%d\n",sizeof(b)/sizeof(b[0]));
-	for(int i=0;i<4;i++){
+	for(i=0;i<4;i++){
 		printf("%d",b[i]);
 	}
 	return 0;
